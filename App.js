89fec60ef5e25ce6c409dd7f2a1634608709d2c2
Carlos Saraiva <carlos.saraiva@msn.com>
2017-04-08 01:40:17 -0300

    Testes inicias com reac-native-elements

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, SideMenu } from 'react-native-elements'
 
 export default class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { isOpen: false }
+  }
+
+  handle = () => {
+    this.setState({isOpen: !this.state.isOpen})
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <SideMenu
+        isOpen={this.state.isOpen}
+        menu={(<Text>Teste</Text>)}
+      >
+        <View style={styles.container}>        
+          <Button
+            large
+            onPress={this.handle}
+            icon={{name: 'squirrel', type: 'octicon', buttonStyle: styles.someButtonStyle }}
+            title='OCTICON' />
+
+          <Text>{this.state.isOpen ? 'Ola': 'Tchau'}</Text>
+        </View>
+      </SideMenu>
     );
   }
 }
